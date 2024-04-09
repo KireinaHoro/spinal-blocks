21715b6bd5d0a7a1717a6f584cf92f2c01a8fbff
Pengcheng Xu <pengxu@ethz.ch>
2024-04-09 13:44:42 +0200

    better naming

--- blocks/jsteward/blocks/axi/AxiCdc.scala
+++ blocks/jsteward/blocks/axi/AxiCdc.scala
@@ -8,11 +8,11 @@ object AxiCdc {
   def apply(push: Axi4, pushClock: ClockDomain, popClock: ClockDomain, depth: Int = 2) = new Composite(push, "axiCdc") {
     val pop = Axi4(push.config) addTag ClockDomainTag(popClock)
 
-    SimpleAsyncFifo(push.ar, pop.ar, depth, pushClock, popClock)
-    SimpleAsyncFifo(push.aw, pop.aw, depth, pushClock, popClock)
-    SimpleAsyncFifo(push.w, pop.w, depth, pushClock, popClock)
+    val ar = SimpleAsyncFifo(push.ar, pop.ar, depth, pushClock, popClock)
+    val aw = SimpleAsyncFifo(push.aw, pop.aw, depth, pushClock, popClock)
+    val w = SimpleAsyncFifo(push.w, pop.w, depth, pushClock, popClock)
 
-    SimpleAsyncFifo(pop.r, push.r, depth, popClock, pushClock)
-    SimpleAsyncFifo(pop.b, push.b, depth, popClock, pushClock)
+    val r = SimpleAsyncFifo(pop.r, push.r, depth, popClock, pushClock)
+    val b = SimpleAsyncFifo(pop.b, push.b, depth, popClock, pushClock)
   }.pop
 }
