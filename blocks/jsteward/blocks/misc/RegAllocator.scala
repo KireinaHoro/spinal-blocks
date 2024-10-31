b54f0c3c0c3f8bde1081151646243d8bd774ca54
Pengcheng Xu <pengxu@ethz.ch>
2024-10-31 10:14:16 +0100

    separate single and block allocate

--- blocks/jsteward/blocks/misc/RegAllocator.scala
+++ blocks/jsteward/blocks/misc/RegAllocator.scala
@@ -8,9 +8,19 @@ trait RegBlockReadBack {
   def apply(name: String, subName: String = ""): BigInt
 }
 
+trait RegBlockAlloc {
+  def apply(name: String, subName: String = ""): BigInt
+  def block(name: String, subName: String = "", count: Int): Seq[BigInt]
+}
+
 class RegAllocatorFactory {
-  private case class RegDesc(baseOffset: BigInt, size: BigInt) {
-    def addr(blockBase: BigInt) = baseOffset + blockBase
+  private case class RegDesc(baseOffset: BigInt, size: BigInt, count: Int) {
+    def addr(blockBase: BigInt, idx: Int = 0) = {
+      assert(idx < count, s"trying to access reg idx $idx, larger than total of $count instances")
+      val ret = baseOffset + blockBase + size * idx
+      // println(f"returning $ret%#x for baseOffset $baseOffset%#x blockBase $blockBase%#x size $size replications $count idx $idx")
+      ret
+    }
   }
 
   private type BlockMap = mutable.LinkedHashMap[String, RegDesc]
@@ -24,8 +34,13 @@ class RegAllocatorFactory {
     private var addrOffset: BigInt = 0
     private var readSensitiveAddrOffset: BigInt = blockLen
 
-    trait RegBlockAlloc {
-      def apply(name: String, subName: String = "", size: BigInt = defaultSize, readSensitive: Boolean = false): BigInt
+    trait FullAlloc {
+      def apply(name: String, subName: String = "", idx: Int = 0, count: Int = 1, size: BigInt = defaultSize, readSensitive: Boolean = false): BigInt
+      def toGeneric = new RegBlockAlloc {
+        def apply(name: String, subName: String): BigInt = FullAlloc.this.apply(name, subName)
+        def block(name: String, subName: String, count: Int) =
+          for (i <- 0 until count) yield FullAlloc.this.apply(name, subName, i, count)
+      }
     }
 
     def readBack(blockIdx: Int): RegBlockReadBack = {
@@ -37,33 +52,51 @@ class RegAllocatorFactory {
       }
     }
 
-    def alloc(base: BigInt, blockIdx: Int): RegBlockAlloc = {
+    def alloc(base: BigInt, blockIdx: Int): FullAlloc = {
       assert(!allocatedBases.exists(_._2 == base), f"base addr $base%#x already allocated for $blockName!")
       assert(!allocatedBases.isDefinedAt(blockIdx), f"block index $blockIdx already allocated for $blockName!")
       allocatedBases.update(blockIdx, base)
 
-      (name: String, subName: String, size: BigInt, readSensitive: Boolean) => {
+      (name: String, subName: String, idx: Int, count: Int, size: BigInt, readSensitive: Boolean) => {
+        val key = genKey(name, subName)
         if (allocatedBases.size > 1) {
-          // this is a recall; ensure definition matches with existing
-          val reg = blockMap(genKey(name, subName))
-          assert(reg.size == size)
-          reg.addr(base)
+          // this is a block recall (e.g. multiple instances of the same plugin)
+          // ensure definition matches with existing
+          val reg = blockMap(key)
+          assert(reg.size == size, "tried to recall register as different size")
+          assert(reg.count == count, "tried to recall register block with different count")
+          reg.addr(base, idx)
         } else {
           // this is a new block
-          val allocated = if (!readSensitive) {
-            val out = addrOffset
-            addrOffset += size
-            assert(addrOffset <= blockLen, f"register alloc overflow block length [$base%#x - ${base + blockLen}%#x]")
-            out
-          } else {
-            val out = readSensitiveAddrOffset - readBusWidth
-            readSensitiveAddrOffset -= readBusWidth
-            assert(readSensitiveAddrOffset >= 0, f"register alloc underflow special align")
-            out
+          // check if we allocated the RegDesc already
+
+          // add one register (single) or all regs in block (replicated)
+          def pushReg(): BigInt = {
+            val r = if (!readSensitive) {
+              val out = addrOffset
+              addrOffset += size * count
+              assert(addrOffset <= blockLen, f"register alloc overflow block length [$base%#x - ${base + blockLen}%#x]")
+              out
+            } else {
+              val out = readSensitiveAddrOffset - readBusWidth * count
+              readSensitiveAddrOffset -= readBusWidth * count
+              assert(readSensitiveAddrOffset >= 0, f"register alloc underflow special align")
+              out
+            }
+            assert(addrOffset <= readSensitiveAddrOffset, "regular reg and special reg overlap")
+            r
           }
-          assert(addrOffset <= readSensitiveAddrOffset, "regular reg and special reg overlap")
-          blockMap.update(s"${genKey(name, subName)}", RegDesc(allocated, size))
-          allocated + base
+
+          blockMap.get(key) match {
+            case None =>
+              // create new RegDesc
+              blockMap.update(key, RegDesc(pushReg(), size, count))
+            case Some(desc) =>
+              // we should expand the RegDesc by exactly one
+              assert(desc.count == count, "tries to read reg with different replication count")
+          }
+
+          blockMap(key).addr(base, idx)
         }
       }
     }
@@ -80,7 +113,14 @@ class RegAllocatorFactory {
       block.allocatedBases.values.foreach { base =>
         println(f"==============")
         block.blockMap.foreach { case (name, desc) =>
-          println(f"[$blockName@$base%#x] ${desc.addr(base)}%#x\t: $name (${desc.size} bytes)")
+          if (desc.count == 1) {
+            // single register
+            println(f"[$blockName@$base%#x] ${desc.addr(base)}%#x\t: $name (${desc.size} bytes)")
+          } else {
+            0 until desc.count foreach { i =>
+              println(f"[$blockName@$base%#x] ${desc.addr(base, i)}%#x\t: $name #$i (${desc.size} bytes)")
+            }
+          }
         }
       }
     }
