c2a13c15e3aea96b60f8e60640d1b79d6650abf1
Pengcheng Xu <pengxu@ethz.ch>
2025-03-11 21:14:23 +0100

    fix dropper

--- blocks/jsteward/blocks/axi/AxiStreamDropFrame.scala
+++ blocks/jsteward/blocks/axi/AxiStreamDropFrame.scala
@@ -4,6 +4,7 @@ import spinal.core._
 import spinal.lib._
 import spinal.lib.bus.amba4.axis._
 
+// TODO: test cases
 case class AxiStreamDropFrame(axisConfig: Axi4StreamConfig) extends Component {
   val io = new Bundle {
     val input = slave(Axi4Stream(axisConfig))
@@ -13,9 +14,13 @@ case class AxiStreamDropFrame(axisConfig: Axi4StreamConfig) extends Component {
 
   assert(axisConfig.useLast, "must enable TLAST or we don't know packet boundary")
 
+  val setDrop = io.drop.valid && io.drop.payload
   val doDrop = Reg(Bool()) init False
-  doDrop.setWhen(io.drop.valid && io.drop.payload)
-  doDrop.clearWhen(io.input.lastFire || (io.drop.valid && !io.drop.payload))
+  doDrop.setWhen(setDrop)
+
+  // don't clear drop when it's just asked for this cycle
+  // prevents letting the second one of two last-only streams escape
+  doDrop.clearWhen((io.input.lastFire && !setDrop) || (io.drop.valid && !io.drop.payload))
 
   io.output << io.input.throwWhen(doDrop)
 }
