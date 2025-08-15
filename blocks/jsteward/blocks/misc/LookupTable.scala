8761308693917f6395d6151e0d281bb8c4758ff5
Pengcheng Xu <pengxu@ethz.ch>
2025-08-15 15:13:44 +0200

    allow generating multiple lookup ports

--- blocks/jsteward/blocks/misc/LookupTable.scala
+++ blocks/jsteward/blocks/misc/LookupTable.scala
@@ -1,95 +1,84 @@
 package jsteward.blocks.misc
 
-import jsteward.blocks.misc.LookupTable.LookupFunc
 import spinal.core._
 import spinal.lib._
 import spinal.lib.misc.pipeline._
 
 import scala.language.postfixOps
 
-object LookupTable {
-  /** (stored value, query) => match? */
-  type LookupFunc[VT <: Data, QT <: Data] = (VT, QT) => Bool
-}
-
-case class LookupTable[
-  VT <: Data,
-  QT <: Data,
-  UT <: Data,
-](valueType: HardType[VT],
-  queryType: HardType[QT],
-  userDataType: HardType[UT],
-  numElems: Int,
-  valueInit: VT => Unit,
-  matchFunc: LookupFunc[VT, QT],
- ) extends Component {
+case class LookupTable[VT <: Data](valueType: HardType[VT], numElems: Int)(valueInit: VT => Unit) extends Component {
   val idxWidth = log2Up(numElems)
 
-  val io = new Bundle {
-    val update = slave(Flow(new Bundle {
-      val value = valueType()
-      val idx = UInt(idxWidth bits)
-    }))
-    val readbackIdx = in(UInt(idxWidth bits))
-    val readback = out(valueType())
-
-    val lookup = slave(Stream(new Bundle {
-      val query = queryType()
-      val userData = userDataType()
-    }))
-    val result = master(Stream(new Bundle {
-      val matched = Bool()
-      val value = valueType()
-      val idx = UInt(idxWidth bits)
-      val userData = userDataType()
-    }))
-  }
+  val update = slave(Flow(new Bundle {
+    val value = valueType()
+    val idx = UInt(idxWidth bits)
+  }))
+  val readbackIdx = in(UInt(idxWidth bits))
+  val readback = out(valueType())
 
   val storage = Vec.fill(numElems)(Reg(valueType()))
   storage.foreach(valueInit)
 
-  when (io.update.fire) {
-    storage(io.update.idx) := io.update.value
+  when (update.fire) {
+    storage(update.idx) := update.value
   }
-  io.readback := storage(io.readbackIdx)
+  readback := storage(readbackIdx)
 
-  val pip = new StagePipeline
-  pip.node(0).arbitrateFrom(io.lookup)
+  def makePort[QT <: Data, UT <: Data](queryType: HardType[QT], userDataType: HardType[UT], name: String = "")
+                                      (matchFunc: (VT, QT) => Bool) = this.rework {
+    val gen = new Area {
+      val lookup = slave(Stream(new Bundle {
+        val query = queryType()
+        val userData = userDataType()
+      }))
+      val result = master(Stream(new Bundle {
+        val matched = Bool()
+        val value = valueType()
+        val idx = UInt(idxWidth bits)
+        val userData = userDataType()
+      }))
 
-  val captureKey = new pip.Area(0) {
-    val QUERY = insert(io.lookup.query)
-    val USER_DATA = insert(io.lookup.userData)
+      val pip = new StagePipeline
+      pip.node(0).arbitrateFrom(lookup)
+
+      val captureKey = new pip.Area(0) {
+        val QUERY = insert(lookup.query)
+        val USER_DATA = insert(lookup.userData)
+      }
+      import captureKey._
+
+      val parallelMatch = new pip.Area(1) {
+        val matchVec = Bits(numElems bits)
+        storage zip matchVec.asBools foreach { case (s, m) =>
+          m := matchFunc(s, QUERY)
+        }
+
+        val MATCH_VEC = insert(matchVec)
+      }
+      import parallelMatch._
+
+      val calcIdx = new pip.Area(2) {
+        val MATCHED = insert(MATCH_VEC.orR)
+        val IDX = insert(OHToUInt(MATCH_VEC))
+        val VALUE = insert(PriorityMux(MATCH_VEC, storage))
+      }
+      import calcIdx._
+
+      pip.node(3).driveTo(result) { case (op, n) =>
+        op.matched := n(MATCHED)
+        op.value := n(VALUE)
+        op.idx := n(IDX)
+        op.userData := n(USER_DATA)
+      }
+
+      pip.build()
+
+      /** Latency of a lookup operation.  Can be used e.g. to delay the payload flow before deciding if
+        * the payload should be dropped.
+        */
+      val lookupLatency = pip.nodes.size - 1
+    }.setName(name)
+
+    (gen.lookup, gen.result, gen.lookupLatency)
   }
-  import captureKey._
-
-  val parallelMatch = new pip.Area(1) {
-    val matchVec = Bits(numElems bits)
-    storage zip matchVec.asBools foreach { case (s, m) =>
-      m := matchFunc(s, QUERY)
-    }
-
-    val MATCH_VEC = insert(matchVec)
-  }
-  import parallelMatch._
-
-  val calcIdx = new pip.Area(2) {
-    val MATCHED = insert(MATCH_VEC.orR)
-    val IDX = insert(OHToUInt(MATCH_VEC))
-    val VALUE = insert(PriorityMux(MATCH_VEC, storage))
-  }
-  import calcIdx._
-
-  pip.node(3).driveTo(io.result) { case (op, n) =>
-    op.matched := n(MATCHED)
-    op.value := n(VALUE)
-    op.idx := n(IDX)
-    op.userData := n(USER_DATA)
-  }
-
-  pip.build()
-
-  /** Latency of a lookup operation.  Can be used e.g. to delay the payload flow before deciding if
-    * the payload should be dropped.
-    */
-  val lookupLatency = pip.nodes.size - 1
 }
